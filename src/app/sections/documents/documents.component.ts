5c79255f1c408d143bc018bf64589c5bf2564b45
VadimKagukin <krimerh313@mail.ru>
2017-02-20 10:25:27 +0300

    fix notification

--- src/app/sections/documents/documents.component.ts
+++ src/app/sections/documents/documents.component.ts
@@ -7,60 +7,45 @@ import { NotificationsService, SimpleNotificationsComponent } from 'angular2-not
   styleUrls: ['./documents.component.css']
 })
 export class DocumentsComponent {
-  constructor( private _service: NotificationsService ) {}
+  constructor( private notificationsService: NotificationsService ) {}
 
   public title: string = 'Проверка';
   public content: string = 'Проверка на примере (№)';
   public type: string = '';
 
-  public options = {
-    timeOut: 5000,
-    lastOnBottom: true,
-    clickToClose: true,
-    maxLength: 0,
-    maxStack: 7,
-    showProgressBar: true,
-    pauseOnHover: true,
-    preventDuplicates: false,
-    preventLastDuplicates: 'visible',
-    rtl: false,
-    animate: 'scale',
-    position: ['right', 'bottom']
-  };
-
   create() {
     switch (this.type) {
       case 'success':
-        let a = this._service.success(this.title, this.content);
+        this.notificationsService.success(this.title, this.content);
         break;
       case 'alert':
-        this._service.alert(this.title, this.content);
+        this.notificationsService.alert(this.title, this.content);
         break;
       case 'error':
-        this._service.error(this.title, this.content);
+        this.notificationsService.error(this.title, this.content);
         break;
       case 'info':
-        this._service.info(this.title, this.content);
+        this.notificationsService.info(this.title, this.content);
         break;
       case 'bare':
-        this._service.bare(this.title, this.content);
+        this.notificationsService.bare(this.title, this.content);
         break;
     }
   }
 
   success(){
-    this._service.success(this.title, this.content, this.type);
+    this.notificationsService.success(this.title, this.content, this.type);
   }
 
   alert(){
-    this._service.alert(this.title, this.content, this.type);
+    this.notificationsService.alert(this.title, this.content, this.type);
   }
 
   error(){
-    this._service.error(this.title, this.content, this.type);
+    this.notificationsService.error(this.title, this.content, this.type);
   }
 
   removeAll() { 
-    this._service.remove();
+    this.notificationsService.remove();
   }
 }
