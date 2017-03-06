80d413a81cab63c649b01c6c0715a85b7b19b080
VadimKagukin <krimerh313@mail.ru>
2017-03-06 16:23:22 +0300

    delete test data

--- src/app/services/subjects/subjects.service.ts
+++ src/app/services/subjects/subjects.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
 
 import { BaseService } from '../base.service';
 import { Service } from '../../decorators/service.decorator';
